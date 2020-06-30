c7ee6e24ccf7d950835a5a395910979035c23d08
Alem Embiale <alemwatch@gmail.com>
2020-06-30 05:58:09 -0400

    initial commit

---
+++ src/main/java/com/alem/vision2020/Vision2020Application.java
@@ -0,0 +1,13 @@
+package com.alem.vision2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vision2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vision2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/alem/vision2020/Vision2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alem.vision2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vision2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
